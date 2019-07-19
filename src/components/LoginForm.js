1ade8c22c4640d8b8e35f0a364b89badd2dd7372
Amy Cen <amy.cen@blackrock.com>
2019-07-19 13:24:11 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,6 +5,7 @@ class LoginForm extends React.Component {
     super();
 
     this.state = {
+<<<<<<< HEAD
       username: '',
       password: ''
     };
@@ -17,10 +18,25 @@ class LoginForm extends React.Component {
   }
 
   updatePwd = (e) => {
+=======
+      username: ''
+      password: ''
+    };
+  }
+  
+  changeUsername = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  changePwd = (e) => {
+>>>>>>> 2109c0be24c6111c9e0ff0f335f852e478e9c131
     this.setState({
       password: e.target.value
     })
   }
+<<<<<<< HEAD
 
   submitForm = (e) => {
     e.preventDefault()
@@ -29,23 +45,37 @@ class LoginForm extends React.Component {
     }
   }
  
+=======
+>>>>>>> 2109c0be24c6111c9e0ff0f335f852e478e9c131
   render() {
     return (
       <form onSubmit={this.submitForm}>
         <div>
           <label>
             Username
+<<<<<<< HEAD
             <input id="username" name="username" type="text" value={this.state.username} onChange={this.updateUsername}  />
+=======
+            <input id="username" name="username" type="text" value: {this.state.username} onChange={changeUsername} />
+>>>>>>> 2109c0be24c6111c9e0ff0f335f852e478e9c131
           </label>
         </div>
         <div>
           <label>
             Password
+<<<<<<< HEAD
             <input id="password" name="password" type="password" value={this.state.password} onChange={this.updatePwd} />
           </label>
         </div>
         <div>
           <button type="submit" >Log in</button>
+=======
+            <input id="password" name="password" type="password" value: {this.state.password} onChange={changePwd}/>
+          </label>
+        </div>
+        <div>
+          <button type="submit" onSubmit={this.props.handleSubmit}>Log in</button>
+>>>>>>> 2109c0be24c6111c9e0ff0f335f852e478e9c131
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -8,8 +8,13 @@ class TwitterMessage extends React.Component {
       message: ''
     };
   }
+<<<<<<< HEAD
 
   updateMsg = (e) => {
+=======
+  
+  updatedMsg = (e) => {
+>>>>>>> 2109c0be24c6111c9e0ff0f335f852e478e9c131
     this.setState({
       message: e.target.value
     })
@@ -19,8 +24,13 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
+<<<<<<< HEAD
         <input type="text" name="message" id="message" value={this.state.message} onChange={this.updateMsg}/>
         <p>Remaining Characters: {this.props.maxChars - this.state.message.length}</p>
+=======
+        <input type="text" name="message" id="message" value={this.state.message onChange={this.updatedMsg} />
+        <p>{this.props.maxChar - this.state.message.length}</p>
+>>>>>>> 2109c0be24c6111c9e0ff0f335f852e478e9c131
       </div>
     );
   }
